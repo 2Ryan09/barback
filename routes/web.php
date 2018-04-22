bae0128f623ac2f5563a9df96f3e42b18a4244e0
Ryan Waldheim <rwaldheim3@gatech.edu>
2018-04-22 00:29:55 -0400

    added .env.example

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
+});
+
+Route::get('/home', function () {
+    return view('pages.home');
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
