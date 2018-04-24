757478dac15d9c0408cb4e1e9ea513a46ef201d3
Ryan Waldheim <rwaldheim3@gatech.edu>
2018-04-23 23:37:11 -0400

    fixed navbar

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,29 +1,23 @@
+@extends('layouts.dashboard')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../storage/app/img/dhgc.jpg">
-
-    <title>BarBack</title>
-</head>
-
-<body>
-
+@section('content')
 <div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
 
-    @include('inc.navbar')
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-    <!-- Main component for a primary marketing message or call to action -->
-    <div class="jumbotron">
-        <h1>Hi!</h1>
-        <p>Hello.
-        </p>
+                    You are logged in!
+                </div>
+            </div>
+        </div>
     </div>
-
-</div> <!-- /container -->
+</div>
+@endsection
