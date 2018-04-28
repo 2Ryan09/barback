9540b8e672574ff1d13ce86bac95a3e3f5eac96c
Ryan Waldheim <rwaldheim3@gatech.edu>
2018-04-28 03:00:39 -0400

    styleci fixes

--- app/Http/Controllers/LCBOController.php
+++ app/Http/Controllers/LCBOController.php
@@ -9,18 +9,16 @@ class LCBOController extends Controller
 {
 	public function __construct()
 	{
-		$this->client = new Client([
-		    'base_uri' => 'http://lcboapi.com/products/',
-		]);
-
-		$this->headers = array('Authorization' => 'Token MDowZWI0NTNmNC00NGJjLTExZTgtYjY4OS04ZmZmYjQyODM5NmU6M2tKTGFEOU1YR05mbkN5QXd2Z3FQQVo3anVsaHNPamtvbXhQ',
-        					   'Accept' => 'application/json');
-
-	}
-    public function getProductInfo($productID) 
-    {
-		$response = $this->client->request('GET', $productID, ['headers' => $this->headers]);
-
-
+        $this->client = new Client([
+            'base_uri' => 'http://lcboapi.com/products/',
+        ]);
+  
+        $this->headers = array('Authorization' => 'Token MDowZWI0NTNmNC00NGJjLTExZTgtYjY4OS04ZmZmYjQyODM5NmU6M2tKTGFEOU1YR05mbkN5QXd2Z3FQQVo3anVsaHNPamtvbXhQ',
+                               'Accept' => 'application/json');
     }
+    
+    public function getProductInfo($productID)
+    {
+        $response = $this->client->request('GET', $productID, ['headers' => $this->headers]);
+     }
 }
