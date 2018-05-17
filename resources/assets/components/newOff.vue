cdd7bd66e6c2bcb3c31519a37bafa916eca7698a
Ryan Waldheim <rwaldheim3@gatech.edu>
2018-05-17 13:23:45 -0400

    changed bws button name

--- resources/assets/components/newOff.vue
+++ resources/assets/components/newOff.vue
@@ -6,7 +6,7 @@
 		    <input class="form-control" v-model="offering.product_id" placeholder="12345" required>
 		  </div>
 		  <div class="col-sm-1">
-		    <button type="button" class="btn btn-primary" @click="getBWS">Find</button>
+		    <button type="button" class="btn btn-primary" @click="getBWS">Lookup</button>
 		    <modal name="getBWSByName" height="500" width="1000">
 		    	<div style="padding: 20px;">
 		    		<bws-vuetable @clicked="getID"></bws-vuetable>
