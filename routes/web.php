5b92d3ab42c5d01125ec3fed028ae4aab8872cc6
Ryan Waldheim <rwaldheim3@gatech.edu>
2018-05-21 03:24:49 -0400

    routing reconfiguring

--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,33 @@
 |
 */
 
-Route::get('/', 'PageController@welcome');
+Route::group(['middleware' => 'web'], function () {
+	Route::get('/', 'PageController@welcome');
 
-Route::get('/test', function () {
-    return view('pages.test');
-});
+	Route::get('/test', function () {
+	    return view('pages.test');
+	});
 
-Route::get('/about', 'PageController@about');
+	Route::get('/about', 'PageController@about');
 
-Route::get('/welcome', 'PageController@welcome');
+	Route::get('/welcome', 'PageController@welcome');
 
-Auth::routes();
-Route::get('auth/logout', 'Auth\LoginController@logout')->name('logout');
+	Auth::routes();
+	Route::get('auth/logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::middleware('auth')->group(function () {
-	Route::get('/admin', 'PageController@admin');
-	Route::get('/admin/locations', 'PageController@adminLocations');
+	Route::middleware('web')->group(function () {
+		Route::get('/admin', 'PageController@admin');
+		Route::get('/admin/locations', 'PageController@adminLocations');
 
-	Route::get('/inventory', 'PageController@inventory')->name('inventory.home');
-	Route::get('/inventory/offerings', 'PageController@offerings')->name('inventory.offerings');
-	Route::get('/inventory/create', 'PageController@invcreate')->name('inventory.create');
-	Route::get('/inventory/offerings/create', 'PageController@offcreate')->name('offerings.create');
+		Route::get('/inventory', 'PageController@inventory')->name('inventory.home');
+		Route::get('/inventory/offerings', 'PageController@offerings')->name('inventory.offerings');
+		Route::get('/inventory/create', 'PageController@invcreate')->name('inventory.create');
+		Route::get('/inventory/offerings/create', 'PageController@offcreate')->name('offerings.create');
 
-	Route::get('/bws/overview', 'PageController@overview')->name('bws.overview');
-	Route::get('/bws/create', 'PageController@bwscreate')->name('bws.create');
+		Route::get('/bws/overview', 'PageController@overview')->name('bws.overview');
+		Route::get('/bws/create', 'PageController@bwscreate')->name('bws.create');
 
-	Route::get('/suppliers', 'PageController@supplier')->name('supplier.overview');
-	Route::get('/suppliers/create', 'PageController@suppliercreate')->name('supplier.create');
+		Route::get('/suppliers', 'PageController@supplier')->name('supplier.overview');
+		Route::get('/suppliers/create', 'PageController@suppliercreate')->name('supplier.create');
+	});
 });
